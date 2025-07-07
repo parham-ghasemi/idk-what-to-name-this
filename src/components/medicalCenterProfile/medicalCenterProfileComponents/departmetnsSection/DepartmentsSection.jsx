32b01e3b96e9376dbbf3cdcd8f5611764e05c1bf
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2025-07-07 17:32:37 +0330

    fixed colors and pictures in DepartmentsSection

--- src/components/medicalCenterProfile/medicalCenterProfileComponents/departmetnsSection/DepartmentsSection.jsx
+++ src/components/medicalCenterProfile/medicalCenterProfileComponents/departmetnsSection/DepartmentsSection.jsx
@@ -4,7 +4,7 @@ import "./DepartmentsSection.scss";
 const deps = [
   {
     title: "بخش جراحی و اتاق عمل",
-    img: "/images/clinic-img.jpg",
+    img: "/images/d3193b76d24e8b35680f63b2bf38b74fa1978adf.jpg",
     items: [
       "اتاق عمل جنرال",
       "اتاق عمل قلب",
@@ -15,7 +15,7 @@ const deps = [
   },
   {
     title: "بخش اورژانس",
-    img: "/images/clinic-img.jpg",
+    img: "/images/b373425746e13840b4c587631090171cc2b68b13.jpg",
     items: [
       "اتاق عمل جنرال",
       "اتاق عمل قلب",
@@ -26,7 +26,7 @@ const deps = [
   },
   {
     title: "بخش بستری",
-    img: "/images/clinic-img.jpg",
+    img: "/images/217f009c4b52403869a98f7861f127afe12a4efd.jpg",
     items: [
       "اتاق عمل جنرال",
       "اتاق عمل قلب",
@@ -37,7 +37,7 @@ const deps = [
   },
   {
     title: "بخش پزشکی هسته‌ای",
-    img: "/images/clinic-img.jpg",
+    img: "/images/961742c1484307906618e7fe8e410c1d4d5ac9b1.jpg",
     items: [
       "اتاق عمل جنرال",
       "اتاق عمل قلب",
@@ -48,7 +48,7 @@ const deps = [
   },
   {
     title: "بخش پرتونگاری و تصویربرداری",
-    img: "/images/clinic-img.jpg",
+    img: "/images/fe05cd718b453e228853ad1a7c0bca00f6909fca.jpg",
     items: [
       "اتاق عمل جنرال",
       "اتاق عمل قلب",
@@ -59,7 +59,7 @@ const deps = [
   },
   {
     title: "بخش آزمایشگاه و آسیب‌شناسی",
-    img: "/images/clinic-img.jpg",
+    img: "/images/f49d7e02c668788cf06af8a95d573ed0daefaf1b.jpg",
     items: [
       "اتاق عمل جنرال",
       "اتاق عمل قلب",
