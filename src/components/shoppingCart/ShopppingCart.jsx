86c0cf6b096805ae743a96259708263ec2b9f00e
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2025-07-14 12:07:24 +0330

    added shopping cart steps container

--- src/components/shoppingCart/ShopppingCart.jsx
+++ src/components/shoppingCart/ShopppingCart.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import "./ShopppingCart.scss";
 import ShoppingCartTitle from "./components/shoppingCartTitle/ShoppingCartTitle";
+import ShoppingCartSteps from "./components/stepsIndicator/ShoppingCartSteps";
 
 const ShopppingCart = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
     <div className="shopping-cart-page">
       <div className="shopping-cart-page__shopping-cart-container">
-        <ShoppingCartTitle title='سبد خرید' icon={'/icons/shopping-cart-black.svg'} />
+        <ShoppingCartTitle
+          title="سبد خرید"
+          icon={"/icons/shopping-cart-black.svg"}
+        />
+
+        <div className="shopping-cart-page__shopping-cart-container__body">
+          <ShoppingCartSteps
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
+        </div>
       </div>
     </div>
   );
